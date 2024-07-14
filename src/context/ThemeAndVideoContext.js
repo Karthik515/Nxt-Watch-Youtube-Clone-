74f04571c1f7dbb19e93efc7a7fbdd234c8e3533
Karthik <nxtwave@ip-10-2-41-222.ap-south-1.compute.internal>
2024-07-14 16:31:54 +0000

    Nxt Watch - Youtube Clone App

---
+++ src/context/ThemeAndVideoContext.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const ThemeAndVideoContext = React.createContext({
+  isDarkTheme: false,
+  savedVideos: [],
+  activeTab: 'Home',
+  toggleTheme: () => {},
+  changeTab: () => {},
+  addVideo: () => {},
+})
+
+export default ThemeAndVideoContext
